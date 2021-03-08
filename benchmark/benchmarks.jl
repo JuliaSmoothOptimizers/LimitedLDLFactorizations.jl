b02fb49c5588fb913e2366d6a905fbdd7c74c309
Monssaf Toukal <tmonssaf@gmail.com>
2021-03-08 12:43:06 -0500

    Setting up files for benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,11 +1,15 @@
 using BenchmarkTools, MatrixMarket
 using LinearAlgebra, SparseArrays, DelimitedFiles
 using LimitedLDLFactorizations
+using Pkg.Artifacts
 
-# download from https://github.com/optimizers/sqd-collection
-# run(`git clone https://github.com/optimizers/sqd-collection.git`)
+# obtain path to SQD collection
+const artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
+ensure_artifact_installed("sqdcollection", artifact_toml)
+const sqd_hash = artifact_hash("sqdcollection", artifact_toml)
+@assert artifact_exists(sqd_hash)
+const sqd_path = joinpath(artifact_path(sqd_hash), "sqd-collection-0.1")
 
-const sqd_path = joinpath(dirname(pathof(LimitedLDLFactorizations)), "..", "benchmark", "sqd-collection")
 subdirs = readdir(sqd_path)
 const formulations = ("2x2", "3x3")
 const iters = (0, 5, 10)
