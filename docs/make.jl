bdc93235fa3a162772f208b275e1d0a7e43dd028
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-04-14 21:11:35 -0400

    adding/updating file: docs/make.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter, LimitedLDLFactorizations
+
+makedocs(
+  modules = [LimitedLDLFactorizations],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "LimitedLDLFactorizations.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/LimitedLDLFactorizations.jl.git")
